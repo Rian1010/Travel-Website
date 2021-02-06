ecdacd25e9c8ceb7b7cbebb0be8dad23a2c60d08
Rian1010 <rtegally1098@gmail.com>
2021-02-06 17:18:32 +0100

    Started with the basics of the picture carousel

--- static/js/main.js
+++ static/js/main.js
@@ -13,6 +13,34 @@ window.onscroll = () => {
     });
 };
 
+const picture = document.querySelector('.carousel-pic');
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+const banner = document.querySelector('.banner');
+let index = 0
+
+const images = ['./static/images/Banner.jpg', './static/images/Turkey.jpg', './static/images/Singapore.jpg'];
+
+banner.style.backgroundImage = `url('${images[index]}')`
+
+
+next.addEventListener('click', () => {
+    index++;
+    if (index >= 3) {
+        index = 0;
+    }
+    banner.style.backgroundImage = `url('${images[index]}')`
+    console.log(index)
+});
+
+prev.addEventListener('click', () => {
+    index--;
+    if (index <= -1) {
+        index = 2;
+    }
+    banner.style.backgroundImage = `url('${images[index]}')`
+    console.log(index)
+});
 
 // Search Functionality
 const currentURL = new URL(window.location);
@@ -38,7 +66,6 @@ const notFound = document.querySelector('.not-found');
 // })
 const searchQuery = () => {
     let query = currentURL.search.split('=')[1];
-    console.log(query)
     destinationsLowerCase = destinationCards;
     const destinationArr = Array.from(destinationsLowerCase);
 
@@ -46,13 +73,6 @@ const searchQuery = () => {
         destinationArr.filter(destination => destination.innerText.includes(query) || destination.innerText.toLowerCase().includes(query) ? destination : destination.style.display = 'none');
         destinationArr.forEach(destination => destination.style.display === 'none' ? notFound.innerHTML = "<h2 style='text-align: center;'>Kein Artikel Gefunden<h2>" : notFound.style.display = 'none')
     }
-
-    // dest.filter(card => card.innerText.includes(query) ? console.log(cards.appendChild(card)) : null);
-    // destinations.filter(destination => destination.title.includes(query) || destination.description.includes(query) ? console.log(destination) : null)
 }
+
 window.addEventListener('DOMContentLoaded', searchQuery());
-
-
-
-
-// destinationCards.forEach(card => {card.innerText.includes(query) ? card : null});
