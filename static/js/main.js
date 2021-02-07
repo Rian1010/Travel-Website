3edcc09d2f413e5c61fd95272677f8d61ca5ab3a
Rian1010 <rtegally1098@gmail.com>
2021-02-07 20:18:06 +0100

    Added a Google Maps API

--- static/js/main.js
+++ static/js/main.js
@@ -11,13 +11,13 @@ window.onscroll = () => {
     img.forEach(e => {
         let speed = e.dataset.speed;
         e.style.transform = `translateY(${scroll * -speed}px)`;
-        banner_title.style.opacity = -scroll / (banner_title.offsetHeight) + 5
+        banner_title.style.opacity = -scroll / (banner_title.offsetHeight) + 4
 
         arrow_right.style.transform = `translateX(${scroll * speed}px)`;
-        arrow_right.style.opacity = -scroll / (banner_title.offsetHeight) + 5
+        arrow_right.style.opacity = -scroll / (banner_title.offsetHeight) + 4
 
         arrow_left.style.transform = `translateX(${scroll * -speed}px)`;
-        arrow_left.style.opacity = -scroll / (banner_title.offsetHeight) + 5
+        arrow_left.style.opacity = -scroll / (banner_title.offsetHeight) + 4
 
         banner.style.transition = '0s';
     });
@@ -31,7 +31,7 @@ const prev = document.getElementById('prev');
 const banner = document.querySelector('.banner');
 
 let index = 0
-const images = ['./static/images/Banner.jpg', './static/images/Singapore.jpg', './static/images/Turkey.jpg'];
+const images = ['./static/images/Banner.jpg', './static/images/Singapore.jpg', './static/images/Indonesia.jpg'];
 banner.style.backgroundImage = `url('${images[index]}')`
 
 next.addEventListener('click', () => {
@@ -59,3 +59,34 @@ prev.addEventListener('click', () => {
     console.log(index)
 });
 
+
+
+// Google Maps API 
+window.initMap = () => {
+    const initMap = () => GoogleMap.initGoogleMap();
+
+    class countryPosition {
+        constructor(lat, lng) {
+            this.lat = lat; 
+            this.lng = lng;
+        }
+    }
+
+    let coordinates
+    let desiredPrice = 900
+    switch(true) {
+        case (desiredPrice <= 800):
+            coordinates = {lat: -31.054483, lng: 24.168686};
+            break;
+        case (desiredPrice > 800):
+            coordinates = {lat: -4.326026, lng: 122.081875};
+            break;
+    }
+    console.log(coordinates);
+
+    const indonesia = new countryPosition(-4.326026, 122.081875);
+    let map = new google.maps.Map(document.getElementById('map'), {
+        center: coordinates,
+        zoom: 5
+      });
+}
