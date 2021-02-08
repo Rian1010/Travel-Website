75109b2691cda2fe3c8174b8cf5bd9b0d2d02c0a
Rian1010 <rtegally1098@gmail.com>
2021-02-08 14:13:56 +0100

    Included an input field for number of days

--- static/js/main.js
+++ static/js/main.js
@@ -73,19 +73,33 @@ window.initMap = () => {
         }
     }
 
+    const daysInput = document.getElementById('numberOfDays');
+    const daysDisplay = document.getElementById('dayDisplay');
+
     const priceInput = document.getElementById('priceInput');
     const priceDisplay = document.getElementById('priceDisplay');
 
-    let desiredPrice = document.getElementById('desiredPrice');
-    const priceBtn = document.getElementById('priceBtn');
+    // let desiredPrice = document.getElementById('desiredPrice');
+    // const priceBtn = document.getElementById('priceBtn');
 
     let coordinates = new countryPosition(35.009778, 38.459732)
     let zoomValue = 3
-    
-    let inputtedPrice
 
-    let priceValue
-    priceDisplay.innerText = priceInput.value + "€"
+    let daysValue = 4;
+    daysDisplay.innerText = daysInput.value;
+
+    const daysUpdate = () => {
+        daysValue = daysInput.value;
+        daysDisplay.innerText = daysValue;
+        mapUpdate();
+    }
+
+    daysInput.addEventListener('change', daysUpdate);
+
+    let inputtedPrice = 2005;
+    let priceValue;
+    priceDisplay.innerText = priceInput.value + "€";
+
     const priceUpdate = () => {
         priceValue = priceInput.value;
         priceDisplay.innerText = priceValue + "€";
@@ -105,15 +119,38 @@ window.initMap = () => {
 
     const mapUpdate = () => {
         switch (true) {
-            case (inputtedPrice <= 800):
-                coordinates = new countryPosition(-31.054483, 24.168686);
+            case (daysValue < 4 && inputtedPrice >= 10 && inputtedPrice <= 500):
+                // Paris
+                coordinates = new countryPosition(48.8566969, 2.3514616);
                 zoomValue = 5;
                 break;
-            case (inputtedPrice >= 801):
-                coordinates = new countryPosition(-4.326026, 122.081875);
+            case (daysValue >= 4 && inputtedPrice >= 10 && inputtedPrice <= 500):
+                // Rome
+                coordinates = new countryPosition(41.8933203, 12.4829321);
+                zoomValue = 5;
+                break;
+            case (daysValue < 4 && inputtedPrice >= 10 && inputtedPrice <= 500):
+                // Sydney
+                coordinates = new countryPosition(-33.8548157, 151.2164539);
+                zoomValue = 5;
+                break;
+            case (daysValue >= 4 && inputtedPrice >= 501 && inputtedPrice <= 1000):
+                // Vancouver
+                coordinates = new countryPosition(49.2608724, -123.1139529);
+                zoomValue = 5;
+                break;
+            case (daysValue < 4 && inputtedPrice >= 1001 && inputtedPrice <= 2000):
+                // Auckland, New Zealand
+                coordinates = new countryPosition(-36.852095, 174.7631803);
+                zoomValue = 5;
+                break;
+            case (daysValue >= 4 && inputtedPrice >= 501 && inputtedPrice <= 1000):
+                // Vancouver
+                coordinates = new countryPosition(51.5073219,-0.1276474);
                 zoomValue = 5;
                 break;
             default:
+                alert("Verzeihung! Es wurden keine Ergebnisse gefunden.")
                 coordinates = new countryPosition(35.009778, 38.459732);
                 zoomValue = 3
         }
