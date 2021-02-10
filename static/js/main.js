353a15e0226340092ed138803656b05f47ac1097
Rian1010 <rtegally1098@gmail.com>
2021-02-10 16:49:03 +0100

    Added markers to the map

--- static/js/main.js
+++ static/js/main.js
@@ -18,6 +18,8 @@ window.onscroll = () => {
 
         arrow_left.style.transform = `translateY(${scroll * -speed}px)`;
         arrow_left.style.opacity = -scroll / (banner_title.offsetHeight) + 4
+
+        e.style.transition = "0s"
     });
 };
 
@@ -83,16 +85,16 @@ window.initMap = () => {
     let zoomValue = 3;
 
     // Display the map
-    const mapDisplay = () => {
-        let map = new google.maps.Map(document.getElementById('map'), {
-            center: coordinates,
-            zoom: zoomValue,
-            gestureHandling: "cooperative",
-        });
-    }
-    mapDisplay()
+    // const mapDisplay = () => {
+    // let map = new google.maps.Map(document.getElementById('map'), {
+    //     center: coordinates,
+    //     zoom: zoomValue,
+    //     gestureHandling: "cooperative",
+    // });
+    // }
+    // mapDisplay()
 
-    
+
     // Update days on days input change
 
     let daysValue = 7;
@@ -140,7 +142,7 @@ window.initMap = () => {
         {
             place: "Paris",
             days: 3,
-            price: 60,
+            price: 59.99,
             lat: 48.8566969,
             lng: 2.3514616
         },
@@ -168,11 +170,12 @@ window.initMap = () => {
     ]
 
     // Find the closest result for user input
-    
+
     const results = document.getElementById('results');
     const resultsHeading = document.getElementById('resultsHeading')
 
     const findClosestValues = (userPrice, userDays) => {
+
         let idxOne = 0;
         let idxTwo = 1;
         let currentPrice;
@@ -182,7 +185,7 @@ window.initMap = () => {
         let currentLng = 38.459732;
 
 
-        
+
         while (idxTwo < countries.length) {
             console.log(idxOne, idxTwo, countries.length)
             let firstPlaceDay = countries[idxOne].days;
@@ -198,7 +201,7 @@ window.initMap = () => {
                 currentLat = countries[idxOne].lat;
                 currentLng = countries[idxOne].lng;
                 coordinates = new countryPosition(currentLat, currentLng)
-                zoomValue = 5;
+                zoomValue = 7;
 
                 console.log(currentPrice, currentDay, currentPlace);
                 idxOne++;
@@ -209,7 +212,7 @@ window.initMap = () => {
                 currentLat = countries[idxTwo].lat;
                 currentLng = countries[idxTwo].lng;
                 coordinates = new countryPosition(currentLat, currentLng);
-                zoomValue = 5;
+                zoomValue = 7;
 
                 console.log(currentPrice, currentDay, currentPlace)
                 idxTwo++;
@@ -220,16 +223,75 @@ window.initMap = () => {
             }
         }
         console.log(currentPrice, currentDay, currentPlace, currentLat, currentLng);
-        mapDisplay()
+        // mapDisplay()
+        let map;
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: coordinates,
+            zoom: zoomValue,
+            gestureHandling: "cooperative",
+        });
+
+        const contentString =
+            '<div id="content">' +
+            '<div id="siteNotice">' +
+            "</div>" +
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+            '<div id="bodyContent">' +
+            "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+            "sandstone rock formation in the southern part of the " +
+            "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+            "south west of the nearest large town, Alice Springs; 450&#160;km " +
+            "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+            "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+            "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+            "Aboriginal people of the area. It has many springs, waterholes, " +
+            "rock caves and ancient paintings. Uluru is listed as a World " +
+            "Heritage Site.</p>" +
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+            "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+            "(last visited June 22, 2009).</p>" +
+            "</div>" +
+            "</div>";
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+        });
+        let marker = new google.maps.Marker({
+            position: {
+                lat: 48.8566969,
+                lng: 2.3514616
+            },
+            map,
+            title: "Paris",
+        });
+
+        function addMarker(coords) {
+            let marker = new google.maps.Marker({
+                position: coords,
+                map,
+                title: "Paris",
+            });
+            marker.addListener("click", () => {
+                infowindow.open(map, marker);
+            });
+        }
+        for (let i=0; i < countries.length; i++) {
+            addMarker({
+                lat: countries[i].lat,
+                lng: countries[i].lng
+            })
+        }
+
+        marker.addListener("click", () => {
+            infowindow.open(map, marker);
+        });
 
         if (currentPlace === undefined) {
             resultsHeading.style.display = 'none';
             results.innerText = "Es wurden keine Ergebnisse für Ihre Angaben gefunden.";
             results.style.color = "red"
-        }
-        else {
+        } else {
             resultsHeading.style.display = 'inline';
-            results.innerText = `Ort: ${currentPlace}, Tage: ${currentDay}, Preis: ${currentPrice}`;
+            results.innerText = `Ort: ${currentPlace}, Tage: ${currentDay}, Preis: ${currentPrice}€`;
             results.style.color = "white"
         }
     }
